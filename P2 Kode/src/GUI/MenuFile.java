fef0cded3d9121057159d8f2b0c3c9c0608e92bb
ScumbagDog <lbrack17@student.aau.dk>
2018-03-22 14:51:21 +0100

    Fjernede imports

--- P2 Kode/src/GUI/MenuFile.java
+++ P2 Kode/src/GUI/MenuFile.java
@@ -1,7 +1,4 @@
 package GUI;
-/*import java.util.AbstractMap.SimpleEntry;
-import java.util.Map.Entry;
-import javafx.application.Application; */
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -9,12 +6,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-/*import javafx.scene.effect.DropShadow;
-import javafx.scene.effect.Effect;
-import javafx.scene.effect.Glow;
-import javafx.scene.effect.SepiaTone;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView; */
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
