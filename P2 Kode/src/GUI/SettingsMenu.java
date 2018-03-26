5f3b3cfa5c49d6e48969c4567226b4b8868b9d7b
Martin <mjch17@student.aau.dk>
2018-03-26 15:39:08 +0200

    Commiting so I can pull, as some files important to intelliJ would be overwritten if they got changed during the pull.

--- P2 Kode/src/GUI/SettingsMenu.java
+++ P2 Kode/src/GUI/SettingsMenu.java
@@ -1,7 +1,10 @@
 package GUI;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -12,6 +15,20 @@ public class SettingsMenu {
         BorderPane generalPane = new BorderPane();
         BorderPane versionPane = new BorderPane();
         generalPane.setBottom(versionPane);
+
+            GridPane dimensions = new GridPane();
+                Text        dimensionWidthText = new Text("Width"),
+                            dimensionHeightText = new Text("Height");
+                TextField   dimensionWidth = new TextField(),
+                            dimensionHeight = new TextField();
+                Button      saveSettings = new Button("Save"),
+                            clearSettings = new Button("Reset Settings");
+                dimensions.getChildren().addAll(dimensionHeightText,
+                                                dimensionWidthText,
+                                                dimensionHeight,
+                                                dimensionWidth,
+                                                saveSettings,
+                                                clearSettings);
 
         Text versionInfo = new Text(version);
         versionPane.setRight(versionInfo);
