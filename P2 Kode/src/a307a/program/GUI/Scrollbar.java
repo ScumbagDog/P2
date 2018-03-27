b9c72f7ab18315f5f135fa742a2a7e1037e242a5
iansjensen <icsj16@student.auu.dk>
2018-03-27 11:48:18 +0200

    Scrollbar

---
+++ P2 Kode/src/a307a/program/GUI/Scrollbar.java
@@ -0,0 +1,36 @@
+package a307a.program.GUI;
+
+import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollBar;
+import javafx.stage.Stage;
+
+public class Scrollbar extends Application {
+
+    final ScrollBar sc = new ScrollBar();
+
+
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Group root = new Group();
+        Scene scene = new Scene(root, 180, 180);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Scrollbar");
+        root.getChildren().addAll(sc);
+
+        sc.setLayoutX(scene.getWidth()-sc.getWidth());
+        sc.setMin(0);
+        sc.setOrientation(Orientation.VERTICAL);
+        sc.setPrefHeight(180);
+        sc.setMax(360);
+        sc.setValue(50);
+
+        primaryStage.show();
+    }
+
+
+}
