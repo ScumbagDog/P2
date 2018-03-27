50f9580e7d51cb87f1dba9c6e4d4615c5ba331b2
Conrad Vasnel <cavasne17@student.aau.dk>
2018-03-27 11:58:36 +0200

    Tilføjet docstrings til MidiSequenceReader. Lavet en doctest i /out.

--- P2 Kode/src/a307a/midilib/parser/MidiSequenceReader.java
+++ P2 Kode/src/a307a/midilib/parser/MidiSequenceReader.java
@@ -1,6 +1,15 @@
 package a307a.midilib.parser;
 
+/**
+ * Represents a midi file being read. Returns different
+ * melodic sequences.
+ */
 public abstract class MidiSequenceReader{
+	/**
+	 * Returns the next note of the sequence across tracks
+	 * @return next note of the sequence.
+	 */
 	abstract Note getNextNote();
 	abstract Note getNextNoteOnChannel(int channel);
+
 }
