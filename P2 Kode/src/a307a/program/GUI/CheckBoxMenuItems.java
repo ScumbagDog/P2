1477b92314dd76eb86d17d41eaf964df12669231
Pdo17 <DominhVS@live.dk>
2018-03-27 10:55:05 +0200

    Menu filen test

--- P2 Kode/src/a307a/program/GUI/CheckBoxMenuItems.java
+++ P2 Kode/src/a307a/program/GUI/CheckBoxMenuItems.java
@@ -1,7 +1,28 @@
 package a307a.program.GUI;
 
-import javax.swing.*;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.VBox;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CheckBoxMenuItems extends JMenuItem {
+    VBox vbox = new VBox();
+    vbox.setTranslateX(50);
+    vbox.setTranslateY(50);
+    CheckBox firstCheckBox = new CheckBox("Algoritme 1");
+    CheckBox secondCheckBox = new CheckBox("Alogritme 2");
+    CheckBox thirdCheckBox = new CheckBox("Algoritme 3");
+    firstCheckBox.setSelected(false);
+    secondCheckBox.setSelected(false);
+    thirdCheckBox.setSelected(false);
 
 }
