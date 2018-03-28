c32128cb12a0e2984bd9c208ff843aa623f41334
Conrad Vasnel <cavasne17@student.aau.dk>
2018-03-28 10:38:22 +0200

    Merge branch 'master' of C:\Users\Conrad\IdeaProjects\P2_ with conflicts.

--- P2 Kode/src/a307a/midilib/parser/MidiSequenceReader.java
+++ P2 Kode/src/a307a/midilib/parser/MidiSequenceReader.java
@@ -1,16 +1,23 @@
 package a307a.midilib.parser;
 
-import java.util.List;
+import javax.sound.midi.Sequence;
 
 /**
  * Represents a midi file being read. Returns different
  * melodic sequences.
  */
 public abstract class MidiSequenceReader{
+	private MidiSequenceReader(){}
+
+	/**
+	 * Starts reading from the specified sequence.
+	 * @param sequence
+	 */
+	public MidiSequenceReader(Sequence sequence){}
 	/**
 	 * Returns the next note of the sequence across tracks
 	 * @return next note of the sequence.
 	 */
-	abstract List<Note> getAllNotes();
-
+	public abstract Note getNextNote();
+	public abstract Note getNextNoteOnChannel(int channel);
 }
--- P2 Kode/src/a307a/midilib/parser/Note.java
+++ P2 Kode/src/a307a/midilib/parser/Note.java
@@ -1,8 +1,12 @@
 package a307a.midilib.parser;
 
+/**
+ * Represents a note.
+ */
 public interface Note{
 	int getPitch();
 	int getVelocity();
 	int getDuration();
 	int getMelodicDuration();
+	int getTick();
 }
