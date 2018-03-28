7c9a9837e25101bc84a1ad38623c0ffab8b31fa0
Conrad Vasnel <cavasne17@student.aau.dk>
2018-03-28 11:23:23 +0200

    Opdateret MidiTools med funktioner der returnerer NGrams.

--- P2 Kode/src/a307a/midilib/parser/MidiTools.java
+++ P2 Kode/src/a307a/midilib/parser/MidiTools.java
@@ -1,5 +1,6 @@
 package a307a.midilib.parser;
 
+import java.io.File;
 import java.util.List;
 
 /**
@@ -11,5 +12,7 @@ public class MidiTools{
 		return null;
 	}
 
-	public static getSequenceReader
+	public static MidiSequenceReader getSequenceReader(File midiFile){
+
+	}
 }
