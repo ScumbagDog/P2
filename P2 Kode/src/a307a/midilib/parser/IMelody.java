d078b108542dfedad3e6d4c15da39268e7d0bb41
Martin <mjch17@student.aau.dk>
2018-04-06 15:25:34 +0200

    getChannels i klassen 'Melody' er nu lavet. Vil prøve at teste det når resten af parser pakken er lavet færdig da jeg ellers ikke kan kompilere.

--- P2 Kode/src/a307a/midilib/parser/IMelody.java
+++ P2 Kode/src/a307a/midilib/parser/IMelody.java
@@ -1,10 +1,13 @@
 package a307a.midilib.parser;
 
+import javax.sound.midi.InvalidMidiDataException;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 
 public interface IMelody extends Iterable<INote>{
 	double getBeat(INote note);
-	Set<Integer> getChannels();
+	Set<Integer> getChannels(File file)throws IOException, InvalidMidiDataException;
 	List<Integer> getIntervals();
 }
