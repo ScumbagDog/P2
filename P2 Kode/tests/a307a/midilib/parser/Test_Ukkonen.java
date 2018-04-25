f12c2c3b6813dfdd46d935223ad8a037e0d56254
Conrad Vasnel <cavasne17@student.aau.dk>
2018-04-25 14:11:09 +0200

    Har tilføjet MonophonicMelody og flyttet en masse funktionalitet over i IMelody (kan ikke refactor til "AMelody" ??)

--- P2 Kode/tests/a307a/midilib/parser/Test_Ukkonen.java
+++ P2 Kode/tests/a307a/midilib/parser/Test_Ukkonen.java
@@ -8,12 +8,13 @@ import org.junit.jupiter.api.TestInstance;
 import javax.sound.midi.InvalidMidiDataException;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class Test_Ukkonen{
-	File file1 = new File("a307a/midilib/parser/Mester-Jakob.mid");
+	File file1 = new File("C:\\Users\\Conrad\\IdeaProjects\\P2_\\P2 Kode\\src\\a307a\\midilib\\parser\\Mester-Jakob.mid");
+	File file2 = new File("C:\\Users\\Conrad\\Desktop\\MIDI\\March i G.mid");
 	AMidiSequenceReader msr1 = MidiTools.getMidiSequenceReader(file1);
+	AMidiSequenceReader msr2 = MidiTools.getMidiSequenceReader(file2);
 
 	public Test_Ukkonen() throws InvalidMidiDataException, IOException{
 	}
@@ -23,6 +24,16 @@ public class Test_Ukkonen{
 	void test1(){
 		IMelody mel1 = msr1.getMelody(0);
 		IMelody mel2 = msr1.getMelody(0);
+		IAlgorithm uk = new Ukkonen(2);
+		double compRes = uk.compareTo(mel1, mel2);
+		System.err.println(compRes);
+	}
+
+	/* Test with very different melody. */
+	@Test
+	void test2(){
+		IMelody mel1 = msr1.getMelody(0);
+		IMelody mel2 = msr2.getMelody(0);
 		IAlgorithm uk = new Ukkonen(2);
 		double compRes = uk.compareTo(mel1, mel2);
 		System.err.println(compRes);
