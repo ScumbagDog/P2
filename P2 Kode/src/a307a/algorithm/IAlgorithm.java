b6195ac5e3aabcfd58eb9248f20257834de2d243
ScumbagDog <lbrack17@student.aau.dk>
2018-04-25 13:59:34 +0200

    Fiksede Conrads rod

--- P2 Kode/src/a307a/algorithm/IAlgorithm.java
+++ P2 Kode/src/a307a/algorithm/IAlgorithm.java
@@ -1,9 +1,9 @@
 package a307a.algorithm;
 
-import a307a.midilib.parser.AMelody2;
+import a307a.midilib.parser.IMelody;
 
 // Compare melody because we have to compare something
 public interface IAlgorithm {
     public String getName();
-    public double compareTo(AMelody2 midiMelody1, AMelody2 midiMelody2);
+    public double compareTo(IMelody midiMelody1, IMelody midiMelody2);
 }
--- P2 Kode/src/a307a/algorithm/Ukkonen.java
+++ P2 Kode/src/a307a/algorithm/Ukkonen.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import a307a.midilib.parser.INGram;
 import a307a.midilib.parser.INGramFactory;
-import a307a.midilib.parser.Melody;
+import a307a.midilib.parser.IMelody;
 import a307a.midilib.parser.NGramFactory;
 /*
  * This class is an implementation of the Ukkonen measure algorithm.
@@ -30,7 +30,7 @@ public class Ukkonen implements IAlgorithm {
 		this.nGramMagnitude = nGramMagnitude;
 	}
 
-	public double compareTo(AMelody2 midiMelody1, AMelody2 midiMelody2) {
+	public double compareTo(IMelody midiMelody1, IMelody midiMelody2) {
 		INGramFactory nGramFactory = new NGramFactory();
 		List<INGram> firstMelodyNGrams = nGramFactory.getNGrams(midiMelody1.getPitchIntervals(), nGramMagnitude);
 		List<INGram> secondMelodyNGrams = nGramFactory.getNGrams(midiMelody2.getPitchIntervals(), nGramMagnitude);
