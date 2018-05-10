3c8684f53d10c524a0903730347ea77c52b3b4ec
Cold Baka <mjch17@student.aau.dk>
2018-05-10 21:39:39 +0200

    Reduceret MainGUI fra ~150 linjer til ~80 linjer ved at samle objekter og metoder i deres egne klasser.

--- P2 Kode/src/a307a/program/GUI/MainGUI.java
+++ P2 Kode/src/a307a/program/GUI/MainGUI.java
@@ -1,5 +1,6 @@
 package a307a.program.GUI;
 
+import a307a.program.GUI.MenuBar.AccessBar;
 import a307a.program.GUI.MenuBar.FileListEditor;
 import a307a.program.GUI.MenuBar.MidiFile;
 import a307a.program.GUI.MenuBar.settings.SettingsFile;
@@ -25,98 +26,32 @@ import java.util.List;
 //This class exists solely to house the 'start' method
 public class MainGUI extends Application {
     private Stage stage = new Stage();
-    private VBox algorithms = new VBox();
-    private int windowHeight, windowWidth;
-    private Boolean windowFullscreen;
-    private List<File> srcFileList = new ArrayList<>(),
-            compFileList = new ArrayList<>();
+    private SettingsFile settings = new SettingsFile();
     private List<CheckBox> listOfAlgorithms = AlgorithmList.listAlgorithm();
-    private List<MidiFile> srcMidiFiles = new ArrayList<>();
-    private List<MidiFile> compMidiFiles = new ArrayList<>();
-    private Text fileName = new Text("Awaiting action...");
-    private BorderPane elementHolder = new BorderPane();
-    private FileList splitLists = new FileList();
-    private MenuBar menuBar = new MenuBar();
-    private Menu menuFile = new Menu("Files");
-    private MenuItem addSrcFile = new MenuItem("Add Source");
-    private MenuItem removeFile = new MenuItem("Remove");
-    private MenuItem addCompFile = new MenuItem("Add Comparison");
-    private Menu menuSetting = new Menu("Window");
-    private MenuItem addSetting = new MenuItem("Settings");
+    private AccessBar accessBar = new AccessBar();
+    GraphicsManager graphicsManager = new GraphicsManager(accessBar);
     private Button compareMelodies = new Button();
     private Resultlist resultlist = new Resultlist();
     private StackPane resultStack1 = new StackPane(resultlist.getTable());
     private StackPane resultStack2 = new StackPane(compareMelodies);
-    private SplitPane resultSplit = new SplitPane();
-    private StackPane algorithmStack = new StackPane(algorithms);
     private Comparison comparison = new Comparison();
-    SettingsFile settings = new SettingsFile();
 
     //Essentially the 'main' method of JavaFX.
     @Override
     public void start(Stage primaryStage) {
         stage.setTitle("Main Menu");
-
-        for(int i = 0; i < listOfAlgorithms.size(); ++i){
-            algorithms.getChildren().add(listOfAlgorithms.get(i));
-        }
-
-        loadSettings();
-        setFileMenuItemFunctionality();
-        setSettingsMenuItemFunctionality();
-        menuBar.getMenus().addAll(menuFile, menuSetting);
+        graphicsManager.loadAlgorithms(listOfAlgorithms);
+        accessBar.setFileMenuItemFunctionality(graphicsManager.getSelectedFiles(), graphicsManager);
         setCompareButtonFunctionality();
         initiateResultList();
 
-        updateDisplay();
-
-        Scene scene = new Scene(elementHolder, windowWidth, windowHeight);
+        graphicsManager.updateDisplay();
+        Scene scene = new Scene(graphicsManager.getElementHolder(), settings.getWindowWidth(), settings.getWindowHeight());
         scene.setFill(Color.OLDLACE);
 
         stage.setScene(scene);
-        stage.setFullScreen(windowFullscreen);
+        stage.setFullScreen(settings.getWindowFullscreen());
         stage.show();
-    }
-
-    private void loadSettings(){
-        windowWidth = Integer.parseInt(settings.AccessSettings("width"));
-        windowHeight = Integer.parseInt(settings.AccessSettings("height"));
-        windowFullscreen = Boolean.parseBoolean(settings.AccessSettings("fullscreen"));
-    }
-
-    private void setFileMenuItemFunctionality(){
-        addSrcFile.setOnAction(event -> { loadFile(srcFileList, srcMidiFiles); });
-        addCompFile.setOnAction(event -> { loadFile(compFileList, compMidiFiles); });
-        removeFile.setOnAction(event -> { new FileListEditor(srcMidiFiles, compMidiFiles, elementHolder); });
-        menuFile.getItems().addAll(addSrcFile, addCompFile, removeFile);
-    }
-
-    private void loadFile(List<File> listOfFiles, List<MidiFile> listOfMidis){
-        listOfFiles.addAll(FileListEditor.AddFile());
-        initiateMidiFileList(listOfFiles, listOfMidis);
-        fileName.setText("File \"" + listOfMidis.get(listOfMidis.size() - 1).getFilePath().getName() + "\" has been added!");
-        elementHolder.setCenter(splitLists.ListsOfFiles(srcMidiFiles, compMidiFiles));
-    }
-
-    private void initiateMidiFileList(List<File> listOfFiles, List<MidiFile> listOfMidis){
-        for(int count = 0; count < listOfFiles.size(); ++count){
-            try {
-                listOfMidis.add(new MidiFile(listOfFiles.get(count)));
-            } catch (InvalidMidiDataException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        listOfFiles.clear();
-    }
-
-    private void setSettingsMenuItemFunctionality(){
-        addSetting.setOnAction(event -> {
-            String version = "???";
-            SettingsMenu.WindowSettings(version);
-        });
-        menuSetting.getItems().add(addSetting);
     }
 
     private void setCompareButtonFunctionality(){
@@ -124,11 +59,13 @@ public class MainGUI extends Application {
         compareMelodies.setText("Compare");
         compareMelodies.setOnAction(event -> {
             Stage confirmAction = new Stage();
-            Text confirmationText = new Text("A total of " + srcMidiFiles.size() + " source files and "
-                    + compMidiFiles.size() + " comparison files have been selected.\n" +
+            Text confirmationText = new Text("A total of " + graphicsManager.getSelectedFiles().getSrcMidiFiles().size() + " source files and "
+                    + graphicsManager.getSelectedFiles().getCompMidiFiles().size() + " comparison files have been selected.\n" +
                     "Do you want to begin the comparison sequence?");
             Button startComparison = new Button("Start");
-            startComparison.setOnAction(event2 -> {comparison.useUkonnen(resultlist, srcMidiFiles, compMidiFiles);});
+            startComparison.setOnAction(event2 -> {comparison.useUkonnen(resultlist,
+                    graphicsManager.getSelectedFiles().getSrcMidiFiles(),
+                    graphicsManager.getSelectedFiles().getCompMidiFiles());});
             Button cancelComparison = new Button("Cancel");
             cancelComparison.setOnAction(event3 -> {confirmAction.close();});
             BorderPane content = new BorderPane();
@@ -141,17 +78,9 @@ public class MainGUI extends Application {
     }
 
     private void initiateResultList(){
-        resultSplit.setOrientation(Orientation.VERTICAL);
-        resultSplit.setDividerPositions(0.9);
+        graphicsManager.getResultSplit().setOrientation(Orientation.VERTICAL);
+        graphicsManager.getResultSplit().setDividerPositions(0.9);
         resultStack2.setMaxSize(100, 100);
-        resultSplit.getItems().addAll(resultStack1, resultStack2);
-    }
-
-    private void updateDisplay(){
-        elementHolder.setTop(menuBar);
-        elementHolder.setLeft(algorithmStack);
-        elementHolder.setBottom(fileName);
-        elementHolder.setCenter(splitLists.ListsOfFiles(srcMidiFiles, compMidiFiles));
-        elementHolder.setRight(resultSplit);
+        graphicsManager.getResultSplit().getItems().addAll(resultStack1, resultStack2);
     }
 }
