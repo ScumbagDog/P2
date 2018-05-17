97d962571a3dc96ef299f960c76dd8d892f7c6a2
Conrad Vasnel <cavasne17@student.aau.dk>
2018-05-17 12:06:19 +0200

    Retter sdfasdfasdfg.

--- P2 Kode/tests/a307a/midilib/parser/Test_TFIDF.java
+++ P2 Kode/tests/a307a/midilib/parser/Test_TFIDF.java
@@ -1,8 +1,7 @@
 package a307a.midilib.parser;
 
-import a307a.algorithm.AStatisticallyInformedAlgorithm;
-import a307a.algorithm.TFIDFRelationAlgorithm;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 
 import javax.sound.midi.InvalidMidiDataException;
 import java.io.File;
@@ -48,17 +47,19 @@ public class Test_TFIDF{
 		assert !melodyCollection.isEmpty();
 	}
 
-	@Test
-	void test1() throws InvalidMidiDataException, IOException{
-		AStatisticallyInformedAlgorithm tfidf = new TFIDFRelationAlgorithm(
-				melodyCollection,
-				2);
-		AMidiSequenceReader msr = MidiTools.getMidiSequenceReader(new File(
-				"P2 " + "Kode/March-i-G.mid"));
-		AMelody m1 = msr.getMelody(0);
-		AMelody m2 = msr.getMelody(1);
-
-		double res = tfidf.compareTo(m1, m2);
-		System.out.println("Comp res: " + res);
-	}
+	//	@Test
+	//	void test1() throws InvalidMidiDataException, IOException{
+	//		AStatisticallyInformedAlgorithm tfidf = new
+	// TFIDFRelationAlgorithm(
+	//				melodyCollection,
+	//				2);+
+	//		AMidiSequenceReader msr = MidiTools.getMidiSequenceReader(new
+	// File(
+	//				"P2 " + "Kode/March-i-G.mid"));
+	//		AMelody m1 = msr.getMelody(0);
+	//		AMelody m2 = msr.getMelody(1);
+	//
+	//		double res = tfidf.compareTo(m1, m2);
+	//		System.out.println("Comp res: " + res);
+	//	}
 }
